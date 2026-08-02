2fe7a9a991c054a438dbaa513388808ca5e91fb1
Shivam Giri <shivamgiri2015@gmail.com>
2026-08-02 08:25:53 +0200

    Adding syncronous Blocking route for application to handle requests in a blocking manner.

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.get('/non-blocking', (req, res) => {
   res.status(200).send('This is Non Blocking Syncronous route');
 });
 
+app.get('/blocking', (req, res) => {
+  // Simulate a blocking operation
+  const start = Date.now();
+  for (let i = 0; i < 1e10; i++);
+  const end = Date.now();
+  res.status(200).send(`Blocking operation took ${end - start} ms`);
+  // My output `Blocking operation took 11284 ms`
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
